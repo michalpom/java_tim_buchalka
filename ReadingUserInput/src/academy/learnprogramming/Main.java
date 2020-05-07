552fee7816acdcaa46f43bded0fa0afecf50f665
michalpom <michalpom@gmail.com>
2020-05-07 21:06:02 +0200

    Exception hangling for ReadingUserInput

--- ReadingUserInput/src/academy/learnprogramming/Main.java
+++ ReadingUserInput/src/academy/learnprogramming/Main.java
@@ -9,20 +9,31 @@ public class Main {
         Scanner scanner = new Scanner(System.in); //creating an instance of Scanner
 
         System.out.println("Enter the year of the birth:");
-        int year =scanner.nextInt(); //reading string and converting it to integer
-        int age= 2020-year;
-        scanner.nextLine(); //handling next line character (enter key), it must be after reading int by nextInt()
+        boolean hasInt =scanner.hasNextInt();
 
-        System.out.println("Enter a name: ");
-        String name = scanner.nextLine(); //calling the method nextLine()
+        if(hasInt) {
+
+            int year = scanner.nextInt(); //reading string and converting it to integer
+            scanner.nextLine(); //handling next line character (enter key), it must be after reading int by nextInt()
+            int age = 2020 - year;
 
 
-        System.out.println("Name: "+name);
-        System.out.println("Age: "+age);
+            System.out.println("Enter a name: ");
+            String name = scanner.nextLine(); //calling the method nextLine()
 
-        if (age>18){
-            System.out.println("You are an adult, you can drink alcohol!");
+            if (age >= 0 && age <= 100) {
+                System.out.println("Name: " + name);
+                System.out.println("Age: " + age);
+                if (age > 18) {
+                    System.out.println("You are an adult, you can drink alcohol!");
+                }
+            } else {
+                System.out.println("Invalid year of birth");
+            }
+        }else {
+            System.out.println("Unable to parse the year of birth. Error :( ");
         }
+
 
         scanner.close();
 
